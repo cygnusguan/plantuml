c337bebabc852068e202c57b66bdb64ff9602042
oldcygnus <oldcygnus@outlook.com>
2021-06-12 22:25:11 +0800

    mend

--- test/net/sourceforge/plantuml/RunTest.java
+++ test/net/sourceforge/plantuml/RunTest.java
@@ -10,6 +10,6 @@ class RunTest {
 
     @Test
     void main() throws IOException, InterruptedException {
-        Run.main(new String[]{"-headless","/Users/cygan/Downloads/江阴POC准备-任务进度-2021-06-12.puml","-tsvg"});
+        Run.main(new String[]{"-headless","/Users/cygan/Downloads/xxx.puml","-tsvg"});
     }
 }
